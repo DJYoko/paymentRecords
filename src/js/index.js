350fbc365c286212df918e1d803c0b10dcc30fd4
dj_yoko <yokochan.recurrent@gmail.com>
2019-04-14 23:04:34 +0900

    add functions for filter payments by datetime

--- src/js/index.js
+++ src/js/index.js
@@ -10,13 +10,32 @@ export class Container extends React.Component {
   }
   render() {
 		const _state = store.getState();
+		const moneyArray = _state.payments.map((payment)=>{
+				return payment.value;
+		})
+		const total = this.sum(moneyArray);
     return (
 			<div>
 				root element here
 				<PaymentList payments={_state.payments} />
+
+				<div className="row">
+					<div className="col-xs-6"><h2>Current Total Money:</h2></div>
+					<div className="col-xs-6"><h2>{total}</h2></div>
+				</div>
 			</div>
     );
   }
+	filterByTimestamp(payments, start, end) {
+		return arr.map(function(payment) {
+				return start < payment.payed_at  && payment.payed_at < end;
+		});
+	}
+	sum(arr) {
+		return arr.reduce(function(prev, current, i, arr) {
+        return prev+current;
+    });
+	}
 }
 
 ReactDOM.render(<Container />, document.getElementById('app'));
