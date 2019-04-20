f541b9d02f2c10300130568c7311c312845f22de
dj_yoko <yokochan.recurrent@gmail.com>
2019-04-20 21:53:23 +0900

    add style for Register cmponent

--- src/js/components/Register/index.js
+++ src/js/components/Register/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
 import PropTypes from 'prop-types';
+import _JSXStyle from 'styled-jsx/style';
+import css from 'styled-jsx/css';
 
 export class Register extends Component {
 	constructor(props) {
@@ -17,15 +18,24 @@ export class Register extends Component {
 	}
 	render() {
 		return (
-			<div className="row">
-				<div className="col-sm-3"><input className="form-control" type="text" name="name" placeholder="ex: income" onChange={this.onChangeName} /></div>
-				<div className="col-sm-3">
-					<div className="input-group"><input className="form-control" type="number" name="value" placeholder="ex: 10000" onChange={this.onChangeValue} />
-						<span className="input-group-addon">円</span></div></div>
-				<div className="col-sm-3"><input className="form-control" type="date" name="payed_at" placeholder="ex: 2019/01/01" onChange={this.onChangePayedAt} /></div>
-				<div className="col-sm-3">
-					<button className="btn btn-primary" onClick={this.onClick}>追加</button>
+			<div className="register">				
+				<div className="row">
+					<div className="col-sm-3">
+						<input className="form-control" type="text" name="name" placeholder="name" onChange={this.onChangeName} />
+					</div>
+					<div className="col-sm-3">
+						<div className="input-group"><input className="form-control" type="number" name="value" placeholder="ex: 10000" onChange={this.onChangeValue} />
+							<span className="input-group-addon">円</span>
+						</div>
+					</div>
+					<div className="col-sm-3">
+						<input className="form-control" type="date" name="payed_at" placeholder="ex: 2019/01/01" onChange={this.onChangePayedAt} />
+					</div>
+					<div className="col-sm-3">
+						<button className="btn btn-primary" onClick={this.onClick}>保存</button>
+					</div>
 				</div>
+				<style jsx>{ styles }</style>
 			</div>
 		);
 	}
@@ -51,3 +61,11 @@ export class Register extends Component {
 	}
 
 }
+
+const styles = css`
+	.register {
+		background-color: #eeeeee;
+		padding: 12px;
+		margin-bottom: 20px;
+	}
+`;
