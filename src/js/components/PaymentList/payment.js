6acca0afc1eca8dc2bf89c9bdd233ac19fccc45e
dj_yoko <yokochan.recurrent@gmail.com>
2019-04-21 00:07:54 +0900

    each payment value dislays comma separated value.

--- src/js/components/PaymentList/payment.js
+++ src/js/components/PaymentList/payment.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import PropTypes from 'prop-types';
+import util from '../../util';
 
 export class Payment extends Component {
 	constructor(props) {
 		super(props);
 	}
 	render() {
+		const displayCost = util.commaSeparate(this.props.value)
 		return (
 			<div className="row" id={this.props.id}>
 				<div className="col-xs-4">{this.props.name}</div>
-				<div className="col-xs-4 text-right">{this.props.value}</div>
+				<div className="col-xs-4 text-right">{displayCost}</div>
 				<div className="col-xs-4">{this.props.payed_at}</div>
 			</div>
 		);
